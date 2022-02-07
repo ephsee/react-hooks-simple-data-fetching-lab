33fea3d92578b2b87b5e11eac4fc12486e0bb74e
Francois Spies <fcspies@gmail.com>
2022-02-07 12:11:42 -0500

    commit tree 136e60ec569f923f9ba00836e4e8cef6473bed89

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+
+import { useEffect, useState } from "react"
+
+
+function App() {
+
+    const [stuff, setStuff] = useState([])
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(resp => resp.json())
+            .then(fetchedStuff => {
+                setStuff( fetchedStuff )
+            })
+    }, [])
+
+    console.log(stuff)
+
+    return (
+        <div>
+        <p>Loading ...</p>
+        <img src={stuff.message} alt="A Random Dog"/>
+        </div>
+    )
+}
+
+export default App
